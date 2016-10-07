967d097882e39623d954e639d2d579b687012736
lenovo <lenovo@idea-PC>
2016-10-07 14:40:15 +0800

    gg

--- src/Solution1.java
+++ src/Solution1.java
@@ -1,4 +1,26 @@
+import java.util.Stack;
 
+/* 题目描述
+用两个栈来实现一个队列，完成队列的Push和Pop操作。
+ 队列中的元素为int类型。
+ */
 public class Solution1 {
+	Stack<Integer> stack1=new Stack<Integer>();
+	Stack<Integer> stack2=new Stack<Integer>();
+	public void push(int code)
+	{
+		stack1.push(code);
+	}
+	public int pop()
+	{
+	if(stack2.empty())
+	{
+		while(!stack1.empty())
+		{
+			stack2.push(stack1.pop());
+		}
+	}
+	return stack2.pop();
+	}
 
 }
